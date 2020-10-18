f91f9685f643d6b8e6948c4c5aee68a4386e2a25
Chrystek <chrystian.kownacki@gmail.com>
2020-10-18 22:59:03 +0200

    Revert "taka"
    This reverts commit 7dff601ddabd865804427cb82f7c33ff2f004d12.

--- allegro_rest_api.py
+++ allegro_rest_api.py
@@ -4,26 +4,26 @@ import pprint
 import webbrowser
 from datetime import datetime, timedelta
 
+class getAccessToken(): 
 
-class getAccessToken():
-
-    authUrl = "https://allegro.pl.allegrosandbox.pl/auth/oauth/token?grant_type=client_credentials"
+    authUrl = "https://allegro.pl.allegrosandbox.pl/auth/oauth/token?grant_type=client_credentials";
     clientId = "07e406aa9455432bb29806aceb0ebea0"
     clientSecret = "6fnrkF5gGd73wGoCPrmZxlva8GgjaxuTHDo7hraNJqkSGNFySFbr1mlXXvXtnZ2N"
 
     ch = curl_init(authUrl)
 
-    curl_setopt(ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC)
-    curl_setopt(ch, CURLOPT_USERNAME, clientId)
-    curl_setopt(ch, CURLOPT_PASSWORD, clientSecret)
-    curl_setopt(ch, CURLOPT_RETURNTRANSFER, 1)
+    curl_setopt(ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt(ch, CURLOPT_USERNAME, clientId);
+    curl_setopt(ch, CURLOPT_PASSWORD, clientSecret);
+    curl_setopt(ch, CURLOPT_RETURNTRANSFER, 1);
 
-    tokenResult = curl_exec(ch)
-    resultCode = curl_getinfo(ch, CURLINFO_HTTP_CODE)
-    curl_close(ch)
+    tokenResult = curl_exec(ch);
+    resultCode = curl_getinfo(ch, CURLINFO_HTTP_CODE);
+    curl_close(ch);
 
-    if (tokenResult == false and resultCode != 200):
+    if (tokenResult == false and resultCode != 200): 
         print("Something went wrong")
+    
 
     tokenObject = json_decode(tokenResult)
 
@@ -35,4 +35,4 @@ class main():
     print("access_token = ", getAccessToken())
 
 
-main():
+main();
